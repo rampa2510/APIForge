1e97285835fe7f26a92202fe982e9610b3ba2242
ram pandey <iamram2510@gmail.com>
2025-04-13 14:44:51 +0530

    feat: gemini model integration

--- gemni_client.py
+++ gemni_client.py
@@ -1,9 +1,13 @@
 import google.generativeai as genai
 
-from config import GEMINI_API_KEY
+from config import GEMINI_API_KEY, MAX_OUTPUT_TOKENS, TEMPERATURE
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash-001")
-response = model.generate_content("The opposite of hot is")
-print(response.text)
+model = genai.GenerativeModel(
+    model_name="gemini-2.0-flash-001",
+    generation_config={
+        "temperature": TEMPERATURE,
+        "max_output_tokens": MAX_OUTPUT_TOKENS,
+    },
+)
